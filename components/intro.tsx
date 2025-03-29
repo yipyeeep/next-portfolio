758b264791a65554854fcbbafa46a61ce9b166f2
dadadadididada <yipyeeep@gmail.com>
2025-03-29 16:47:51 +0800

    looks good, changing file names now

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,18 +5,18 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Hamed.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m HongYip.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Toronto, Canada. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+          I&#39;m a fullstack web developer passionate about 
+          creating seamless, user-friendly applications and 
+          am eager to contribute to innovative projects!
         </p>
       </div>
       <div className='relative'>
         <Image
-          className='flex-1 rounded-lg grayscale'
+          className='flex-1 rounded-lg'
           src={authorImage}
-          alt='Hamed Bahram'
+          alt='Me & my waifu'
           width={175}
           height={175}
           priority
